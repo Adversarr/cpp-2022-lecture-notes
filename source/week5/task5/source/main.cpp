fbe037f156d2fd0715637fe553d18ab488dc5f4d
Zherui Yang <yangzherui2001@foxmail.com>
2022-10-14 14:32:36 +0800

    fix pi

--- source/week5/task5/source/main.cpp
+++ source/week5/task5/source/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cmath>
+
 int main() {
   double pi = 0, sign = 1;
-  for (int i = 1; i < 100000; i += 2) {
+  int i = 1;
+  while (fabs(sign / i) > 1e-7) {
     pi += sign / i;
     sign = - sign;
+    i += 2;
   }
   std::cout << "Pi = " << pi * 4 << std::endl;
   return 0;
